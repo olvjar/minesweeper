040c0e5610b35e2ac165c86b7a9d621d517d0c55
nMariel <160820397+nMariel@users.noreply.github.com>
2024-03-14 13:37:04 +0800

    Update snapshot.c

--- snapshot.c
+++ snapshot.c
@@ -27,59 +27,80 @@ struct player{
 };
 
 
-int saveSnapshot(struct player profile, game level, char outcome[], int n){
+int saveSnapshot(game level, char outcome[]){
 	int i, j;
 	char filename[20];
     char path[] = "recent games/";
     FILE *fgame;
     
     strcpy(filename, "recentgame");
-    if (n == 1) strcat(filename, "1");
-    if (n == 2) strcat(filename, "2");
-    if (n == 3) strcat(filename, "3");
 	strcat(filename, ".txt");
     strcat(path, filename);
     
     fgame = fopen(path, "w");
     
     if (strcmp(outcome, "win") == 0){
+		fprintf(fgame, "GAME WON\n");
+		
 		for (i = 0; i < level.rows; i++){
     		for (j = 0; j < level.cols; j++){
-    			if (level.gameBoard[i][j] != 10){
-				//	fprintf(fgame, (char) level.gameBoard[i][j]);
-    			}
-    			else fprintf(fgame, "X");
+    			
+    			if (level.gameBoard[i][j] != 100 && level.board[i][j] != 'X'){ // tile != flag && tile != bomb
+					fprintf(fgame, " %d ", level.gameBoard[i][j]);
+				}
+				else if (level.board[i][j] == 'X' || level.gameBoard[i][j] == 100){ // tile == bomb || tile == flag (game only wins if all tiles are shown, so a flag == bomb)
+					fprintf(fgame, " X ");
+				}
 		}
 		fprintf(fgame, "\n");
 	}
 }
 	
 	else if (strcmp(outcome, "lose") == 0){
+		fprintf(fgame, "GAME LOST\n");
+		
 		for (i = 0; i < level.rows; i++){
     		for (j = 0; j < level.cols; j++){
-    			if (level.gameBoard[i][j] != 10 && level.gameBoard[i][j] != 100){ // if tile is revealed
-				//	fprintf(fgame, (char) level.gameBoard[i][j]);
+    			if (level.gameBoard[i][j] != 10 && level.gameBoard[i][j] != 100 && level.gameBoard[i][j] != 999){ // tile != hidden && tile != flag && tile != bombExploded
+					fprintf(fgame, " %d ", level.gameBoard[i][j]);
 				}
-				if (level.gameBoard[i][j] == 10){ // tile == not revealed
-					fprintf(fgame, ".");
-    			}
-    			else if (level.gameBoard[i][j] == 100){ // tile == flag
-    				fprintf(fgame, "F");
+				else if(level.gameBoard[i][j] == 999){ // bomb == exploded
+					fprintf(fgame, " X ");
 				}
 				else if (level.board[i][j] == 'X'){ // tile == bomb
-					fprintf(fgame, "x");
+					fprintf(fgame, " x ");
 				}
-				else fprintf(fgame, "X");
-		}
+				else if (level.gameBoard[i][j] == 100){ // tile == flag
+    				fprintf(fgame, " F ");
+				}
+				else if (level.gameBoard[i][j] == 10){ // tile == not revealed
+					fprintf(fgame, " . ");
+    			}
+			}
 		fprintf(fgame, "\n");
-	}
+		}
 }
 	
-	else if (strcmp(outcome, "pause") == 0){
+	else if (strcmp(outcome, "quit") == 0){
+		fprintf(fgame, "GAME QUIT\n");
+		
+		for(i = 0; i < level.rows; i++){
+			for(j = 0; j < level.cols; j++){
+				
+				if(level.gameBoard[i][j] == 10){
+					fprintf(fgame, " . ");
+				}
+				else if (level.gameBoard[i][j] == 100){
+					fprintf(fgame, " F ");
+				}
+				else fprintf(fgame, " %d ", level.gameBoard[i][j]);
+			}
+		fprintf(fgame, "\n");
+		}
 	}
     
     fclose(fgame);
     
-    
+    return 1;
 }
 
