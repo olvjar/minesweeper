744e4a291e14783240f338137744904f497093d3
olvjar <43496985+olvjar@users.noreply.github.com>
2024-05-14 11:54:12 +0800

    chore: rm

--- main.c
+++ main.c
@@ -2836,15 +2836,3 @@ int main(){
 		delay(1000);
 	}
 }
-
-/*
-	This is to certify that this project is my own work, based on my personal
-	efforts in studying and applying the concepts learned. I have constructed
-	the functions and their respective algorithms and corresponding code by
-	myself. The program was run, tested, and debugged by my own efforts.
-	I further certify that I have not copied in part or whole or otherwise
-	plagiarized the work of other students and/or persons.
-
-	GRINO, MARY EUNICE E., DLSU ID# 12325872
-	TAMONDONG, MARIEL M., DLSU ID# 12308323
-*/
